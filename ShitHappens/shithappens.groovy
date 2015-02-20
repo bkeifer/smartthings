2f10643dee5549261a012c358eca383aca51d47c
Brian Keifer <brian@valinor.net>
2015-02-19 19:17:21 -0500

    Basic working version

--- ShitHappens/shithappens.groovy
+++ ShitHappens/shithappens.groovy
@@ -1,18 +1,18 @@
 /**
- *  Shit Happens
- *
- *  Copyright 2015 Brian Keifer
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- */
+*  Shit Happens
+*
+*  Copyright 2015 Brian Keifer
+*
+*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License. You may obtain a copy of the License at:
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+*  for the specific language governing permissions and limitations under the License.
+*
+*/
 definition(
     name: "Shit Happens",
     namespace: "bkeifer",
@@ -24,135 +24,199 @@ definition(
     iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
 
 
-preferences {
-    section("Control these bulbs...") {
+    preferences {
+        section("Control these bulbs...") {
             input "hues", "capability.colorControl", title: "Which Hue Bulbs?", required:true, multiple:true
+        }
+        section("Light level...") {
+            input "level", "number", title: "In percent", required: false
+        }
+        section("Warn for these contact sensors...") {
+            input "contacts", "capability.contactSensor", title: "Contacts", required: false, multiple: true
+        }
+        section("Low temperature to trigger warning...") {
+            input "lowtemp", "number", title: "In degrees F", required: false
+        }
+        section("Forecast API Key") {
+            input "apikey", "text", required: false
+        }
     }
-    section("Warn for these contact sensors...") {
-        input "contacts", "capability.contactSensor", title: "Contacts", required: false, multiple: true
+
+    def installed() {
+        log.debug "Installed with settings: ${settings}"
+        initialize()
     }
-    section("Low temperature to trigger warning...") {
-        input "lowtemp", "number", title: "In degrees F", required: false
+
+    def updated() {
+        log.debug "Updated with settings: ${settings}"
+        unsubscribe()
+        initialize()
     }
-    section("Forecast API Key") {
-        input "apikey", "text", required: false
+
+    def initialize() {
+        unschedule()
+        log.trace("subscribing to app/apptouch")
+        state.alerts = [:]
+        state.contacts = [:]
+
+        subscribe(app, appTouch)
+        subscribe(contacts, "contact.open", contactOpenHandler)
+        subscribe(contacts, "contact.closed", contactClosedHandler)
+
+        runEvery5Minutes(checkAll)
     }
-}
-
-def installed() {
-    log.debug "Installed with settings: ${settings}"
-
-    initialize()
-}
-
-def updated() {
-    log.debug "Updated with settings: ${settings}"
-
-    unsubscribe()
-    initialize()
-}
-
-def initialize() {
-    // TODO: subscribe to attributes, devices, locations, etc.
-    log.trace("subscribing to app/apptouch")
-    subscribe(app, appTouch)
-}
 
 
-def turnOnToColor(color) {
-    def hueColor = 70
-    def saturation = 100
-    switch(color) {
-            case "Blue":
+    def contactOpenHandler(evt) {
+        log.debug("Door opened")
+        state.contacts[evt.displayName] = false
+        state.alerts["contact"] = true
+
+        //turnOnToColor("Red")
+        checkAll()
+        log.trace(state)
+    }
+
+
+    def contactClosedHandler(evt) {
+        int openContacts = 0
+        log.debug("Door closed")
+        state.contacts[evt.displayName] = true
+        state.contacts.each {
+            log.debug(evt)
+            log.debug("CONTACT: ${it.key} - ${it.value}")
+            if (it.value == false) {
+                openContacts++
+            }
+        }
+        if (openContacts > 0) {
+            log.debug("Doors still open")
+
+            //turnOnToColor("Red")
+            } else {
+                log.debug("All doors closed")
+                state.alerts["contact"] = false
+                //turnOnToColor("Green")
+            }
+            checkAll()
+            log.trace(state)
+        }
+
+
+        def turnOnToColor(color) {
+            def hueColor = 70
+            def saturation = 100
+            def lightLevel = 100
+            switch(color) {
+                case "Blue":
                 hueColor = 70
                 break;
-            case "Green":
+                case "Green":
                 hueColor = 39
                 break;
-            case "Yellow":
+                case "Yellow":
                 hueColor = 25
                 break;
-            case "Orange":
+                case "Orange":
                 hueColor = 10
                 break;
-            case "Purple":
+                case "Purple":
                 hueColor = 75
                 break;
-            case "Pink":
+                case "Pink":
                 hueColor = 83
                 break;
-            case "Red":
+                case "Red":
                 hueColor = 100
                 break;
-    }
-    if (color != "On - Custom Color")
-    {
-        def newValue = [hue: hueColor, saturation: saturation, level: lightLevel as Integer ?: 100]
-        hues*.setColor(newValue)
-        log.debug "new value = $newValue"
-    }
-    else
-    {
-        hues*.on()
-    }
-}
+            }
+            if (color != "On - Custom Color") {
+                def newValue = [hue: hueColor, saturation: saturation, level: lightLevel as Integer ?: 100]
+                hues*.setColor(newValue)
+                log.debug "new value = $newValue"
+                } else {
+                    hues*.on()
+                }
+            }
 
-def getLowTemp() {
-    def params = [
-        uri: "https://api.forecast.io",
-        path: "/forecast/${apikey}/40.496754,-75.438682"
-    ]
-    int lowForecast = 1000  // Ow!
+            def getLowTemp() {
+                def params = [
+                uri: "https://api.forecast.io",
+                path: "/forecast/${apikey}/40.496754,-75.438682"
+                ]
+                int lowForecast = 1000  // Ow!
 
-    try {
-        httpGet(params) { resp ->
-            if (resp.data) {
-                //log.debug "Response Data = ${resp.data}"
-                //log.debug "Response Status = ${resp.status}"
+                try {
+                    httpGet(params) { resp ->
+                        if (resp.data) {
+                            //log.debug "Response Data = ${resp.data}"
+                            //log.debug "Response Status = ${resp.status}"
 
-                // resp.headers.each {
-                //     log.debug "header: ${it.name}: ${it.value}"
-                // }
-                resp.getData().each {
-                    if (it.key == "hourly") {
-                        def x = it.value
-                        x.each { xkey ->
-                            if (xkey.key == "data") {
-                                def y = xkey.value
-                                def templist = y["temperature"][0..24]
-                                templist.each { hourlyTemp ->
-                                    if (hourlyTemp < lowForecast) {
-                                        log.info("new low: ${hourlyTemp}")
-                                        lowForecast = hourlyTemp
+                            // resp.headers.each {
+                            //     log.debug "header: ${it.name}: ${it.value}"
+                            // }
+                            resp.getData().each {
+                                if (it.key == "hourly") {
+                                    def x = it.value
+                                    x.each { xkey ->
+                                        if (xkey.key == "data") {
+                                            def y = xkey.value
+                                            def templist = y["temperature"]
+                                            def timelist = y["time"]
+
+                                            for (int i = 0; i <= 12; i++) {
+                                                if ( templist[i] < lowForecast) {
+                                                    lowForecast = templist[i]
+                                                    log.info("new low: ${templist[i]} at ${new Date( ((long)timelist[i])*1000 ) }")
+                                                }
+                                            }
+                                        }
                                     }
                                 }
                             }
                         }
+                        if(resp.status == 200) {
+                            log.debug "Request was OK"
+                            } else {
+                                log.error "Request got http status ${resp.status}"
+                            }
+                        }
+                        } catch(e) {
+                            log.debug e
+                        }
+                        return lowForecast
                     }
-                }
-            }
-            if(resp.status == 200) {
-                log.debug "Request was OK"
-            }
-            else {
-                log.error "Request got http status ${resp.status}"
-            }
-        }
-    } catch(e) {
-        log.debug e
-    }
-    return lowForecast
-}
 
 
-def appTouch(evt) {
-    //hues*.off()
+                    def appTouch(evt) {
+                        checkAll()
+                        log.trace(state)
+                    }
 
-    log.trace("Getting forecast")
-    if (getLowTemp() < lowtemp ) {
-        log.debug("Freeze warning found, turning on lights.")
-        turnOnToColor("Blue")
-    } else {
-        log.debug("Nothing to write home about.")
-    }
-}
+
+                    def checkAll() {
+                        checkForFreeze()
+                        if(state.alerts["contact"]) {
+                            turnOnToColor("Orange")
+                            } else if (state.alerts["freeze"]) {
+                                turnOnToColor("Blue")
+                                } else {
+                                    hues*.off()
+                                }
+                            }
+
+
+                            def checkForFreeze() {
+                                //hues*.off()
+                                log.trace("Getting forecast")
+                                if (getLowTemp() < lowtemp ) {
+                                    log.debug("Freeze warning found, turning on lights.")
+                                    state.alerts["freeze"] = true
+                                    //turnOnToColor("Blue")
+                                    } else {
+                                        state.alerts["freeze"] = false
+                                        log.debug("The pipes are safe.  For now...")
+                                    }
+                                    log.trace(state)
+                                }
+                                
