e50c75efaecfe0204684d42c9dc714d0c44eced4
Brian Keifer <brian@valinor.net>
2015-10-05 21:50:25 -0400

    Initial commit of Smart Porch Lights

---
+++ SmartPorchLights.groovy
@@ -0,0 +1,120 @@
+/**
+ *  Smart Porch Lights
+ *
+ *  Copyright 2015 Brian Keifer
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+definition(
+    name: "Smart Porch Lights",
+    namespace: "bkeifer",
+    author: "Brian Keifer",
+    description: "Porch lights that brighten temporarily upon your arrival.",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+
+
+preferences {
+	section("Lights:") {
+    	input "lights", "capability.switchLevel", multiple: true, required: true
+	}
+	section("Presence Sensor:") {
+    	input "presenceSensor", "capability.presenceSensor", multiple: true, required: true
+	}
+    section("For how long should we brighten the lights?") {
+		input "brighten", "number", title: "Minutes", defaultValue: 5, required: true
+	}
+    section("What level should the lights be set at normally?") {
+        input "lvlDefault", "number", title: "1-100", defaultValue: 20, required: true
+    }
+    section("What level should the lights brighten to upon arrival?") {
+        input "lvlArrival", "number", title: "1-100", defaultValue: 100, required: true
+    }
+    section("Send Notifications?") {
+        input("recipients", "contact", title: "Send notifications to") {
+            input "phone", "phone", title: "Warn with text message (optional)",
+                description: "Phone Number", required: false
+        }
+    }
+}
+
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+	initialize()
+}
+
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+
+def initialize() {
+	subscribe(presenceSensor, "presence", arrivalHandler)
+    subscribe(location, "sunriseTime", sunriseHandler)
+    subscribe(location, "sunsetTime", sunsetHandler)
+    subscribe(app, sunsetHandler)
+}
+
+
+def sunriseHandler(evt) {
+    lights*.off()
+}
+
+
+def sunsetHandler(evt) {
+    lights*.setLevel(lvlDefault)
+}
+
+
+def arrivalHandler(evt) {
+    if (evt.value == "present") {
+        lights?.setLevel(lvlArrival)
+        run
+    }
+}
+// def presenceHandler(evt) {
+//
+// 	if(evt.value == "present" && state.currentPresence == "not present") {
+//     //if(true) {
+// 		def gracePeriod = now() - ( grace * 60 * 1000 )
+//         log.debug("diff: ${diff}")
+//
+//         if (gracePeriod > lastOpen) {
+// 		    if (garageDoorSensor.latestValue("contact") == "closed") {
+// 			    log.debug("Opening door!")
+//                 state.lastOpen = now()
+// 			    relay.on()
+//             }
+//         }
+//     	state.currentPresence = evt.value
+// 	} else if (evt.value == "not present") {
+//     	log.debug("Updating presenceLeft")
+//     	state.currentPresence = evt.value
+// 		state.presenceLeft = now()
+// 	}
+// }
+//
+
+def notify(msg) {
+    if (location.contactBookEnabled && recipients) {
+        sendNotificationToContacts(msg, recipients)
+    } else if (phone) {
+        sendSms(phone, msg)
+    }
+
+}
