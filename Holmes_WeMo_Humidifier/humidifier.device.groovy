cc67fff2fd854d3c72ed6769b3c7e703afc0c153
Brian Keifer <brian@valinor.net>
2016-01-26 23:24:56 -0500

    UI Overhaul

--- Holmes_WeMo_Humidifier/humidifier.device.groovy
+++ Holmes_WeMo_Humidifier/humidifier.device.groovy
@@ -21,8 +21,13 @@ metadata {
 		capability "Relative Humidity Measurement"
 		capability "Switch"
 
-        attribute "fanMode", "string"
-        attribute "setpoint", "string"
+        attribute "fanMode", "number"
+        attribute "desiredHumidity", "string"
+        attribute "waterAdvise", "number"
+        attribute "noWater", "number"
+        attribute "filterLife", "number"
+        attribute "expiredFilterTime", "number"
+        attribute "randomAttribute", "string"
 
         command "sendFanCommand"
         command "fanMax"
@@ -43,11 +48,32 @@ metadata {
 		// TODO: define status and reply messages here
 	}
 
-	tiles {
-        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
-            state "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
-            state "off", label:'${name}', action:"switch.off", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
-        }
+	tiles (scale: 2){
+        // multiAttributeTile(name:"humidifierMulti", type: "generic", width: 6, height: 4) {
+        //     tileAttribute("device.fanMode", key: "PRIMARY_CONTROL") {
+        //         attributeState("default", label: '${currentValue}')
+        //         attributeState("0",   backgroundColor:"#888888")
+        //         attributeState("20",  backgroundColor:"#B6CCFA")
+        //         attributeState("40",  backgroundColor:"#95B6F8")
+        //         attributeState("60",  backgroundColor:"#75A0F7")
+        //         attributeState("80",  backgroundColor:"#548AF6")
+        //         attributeState("100", backgroundColor:"#3474F5")
+        //     }
+        //     tileAttribute("device.desiredHumidity", key: "VALUE_CONTROL") {
+        //         attributeState("default", action: "setDesiredHumidity")
+        //     }
+        //     tileAttribute("device.humidity", key: "SECONDARY_CONTROL") {
+        //         attributeState("default", label:'Current Humidity: ${currentValue}%', unit:"%")
+        //     }
+        //     tileAttribute("device.fanMode", key: "SLIDER_CONTROL") {
+        //         attributeState("level", action: "sendFanCommand")
+        //     }
+        // }
+
+        // standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+        //     state "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
+        //     state "off", label:'${name}', action:"switch.off", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+        // }
 
         /*
         valueTile("mode", "device.mode",decoration:"flat", inactiveLabel: false) {
@@ -61,39 +87,63 @@ metadata {
 
         standardTile("off", "device.fanMode",label:"Off") {
           state "default", label: 'OFF', action: "fanOff", icon:"st.Appliances.appliances11",backgroundColor:"#ffffff"
-          state "off", label: 'OFF', action: "fanOff", icon:"st.Appliances.appliances11",backgroundColor:"#66ff33"
+          state "0", label: 'OFF', action: "fanOff", icon:"st.Appliances.appliances11",backgroundColor:"#888888"
         }
 
         standardTile("min", "device.fanMode",label:"Min") {
           state "default", label: 'MIN', action: "fanMin", icon:"st.Appliances.appliances11",backgroundColor:"#ffffff"
-          state "min", label: 'MIN', action: "fanMin", icon:"st.Appliances.appliances11",backgroundColor:"#66ff33"
+          state "20", label: 'MIN', action: "fanMin", icon:"st.Appliances.appliances11",backgroundColor:"#B6CCFA"
         }
 
         standardTile("low", "device.fanMode",label:"Low") {
           state "default", label: 'LOW', action: "fanLow", icon:"st.Appliances.appliances11",backgroundColor:"#ffffff"
-          state "low", label: 'LOW', action: "fanLow", icon:"st.Appliances.appliances11",backgroundColor:"#66ff33"
+          state "40", label: 'LOW', action: "fanLow", icon:"st.Appliances.appliances11",backgroundColor:"#95B6F8"
         }
 
         standardTile("med", "device.fanMode") {
           state "default", label: 'MED', action: "fanMed", icon:"st.Appliances.appliances11",backgroundColor:"#ffffff"
-          state "med", label: 'MED', action: "fanMed", icon:"st.Appliances.appliances11",backgroundColor:"#66ff33"
+          state "60", label: 'MED', action: "fanMed", icon:"st.Appliances.appliances11",backgroundColor:"#75A0F7"
         }
 
         standardTile("high", "device.fanMode") {
           state "default", label: 'HIGH', action: "fanHigh", icon:"st.Appliances.appliances11",backgroundColor:"#ffffff"
-          state "high", label: 'HIGH', action: "fanHigh", icon:"st.Appliances.appliances11",backgroundColor:"#66ff33"
+          state "80", label: 'HIGH', action: "fanHigh", icon:"st.Appliances.appliances11",backgroundColor:"#548AF6"
         }
 
         standardTile("max", "device.fanMode",label:"Max") {
           state "default", label: 'MAX', action: "fanMax", icon:"st.Appliances.appliances11",backgroundColor:"#ffffff"
-          state "max", label: 'MAX', action: "fanMax", icon:"st.Appliances.appliances11",backgroundColor:"#66ff33"
+          state "100", label: 'MAX', action: "fanMax", icon:"st.Appliances.appliances11",backgroundColor:"#3474F5"
         }
 
-//st.Weather.weather12
-
-        valueTile("fanMode", "device.fanMode",decoration:"flat") {
-            state "default", label:'Fan Speed: ${currentValue}'
+        standardTile("hum45", "device.desiredHumidity", label:"45%") {
+            state "default", label: "45%", action: "hum45", icon:"st.Weather.weather12", backgroundColor:"#ffffff"
+            state "45%", label: "45%", action: "hum45", icon:"st.Weather.weather12", backgroundColor:"#3333ff"
         }
+
+        standardTile("hum50", "device.desiredHumidity", label:"50%") {
+            state "default", label: "50%", action: "hum50", icon:"st.Weather.weather12", backgroundColor:"#ffffff"
+            state "50%", label: "50%", action: "hum50", icon:"st.Weather.weather12", backgroundColor:"#3333ff"
+        }
+
+        standardTile("hum55", "device.desiredHumidity", label:"55%") {
+            state "default", label: "55%", action: "hum55", icon:"st.Weather.weather12", backgroundColor:"#ffffff"
+            state "55%", label: "55%", action: "hum55", icon:"st.Weather.weather12", backgroundColor:"#3333ff"
+        }
+
+        standardTile("hum60", "device.desiredHumidity", label:"60%") {
+            state "default", label: "60%", action: "hum60", icon:"st.Weather.weather12", backgroundColor:"#ffffff"
+            state "60%", label: "60%", action: "hum60", icon:"st.Weather.weather12", backgroundColor:"#3333ff"
+        }
+
+        standardTile("humMax", "device.desiredHumidity", label:"Max") {
+            state "default", label: "Max", action: "humMax", icon:"st.Weather.weather12", backgroundColor:"#ffffff"
+            state "Max", label: "Max", action: "humMax", icon:"st.Weather.weather12", backgroundColor:"#3333ff"
+        }
+
+
+        // valueTile("fanMode", "device.fanMode",decoration:"flat") {
+        //     state "default", label:'Fan Speed: ${currentValue}'
+        // }
 
 //        valueTile("filterLife", "device.filterLife",decoration:"flat") {
 //            state "default", label:'Filter Life:${currentValue}%'
@@ -107,26 +157,47 @@ metadata {
 //            state "default", label:'Desired Humidity: ${currentValue}', unit:"%"
 //        }
 
-        standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+
+        standardTile("refresh", "device.refresh", width: 2, height: 2, inactiveLabel: false, decoration: "flat") {
             state "default", label:'', action:"refresh.refresh", icon:"st.secondary.refresh"
         }
 
-        controlTile("fanSliderControl", "device.fanMode", "slider", height: 1, width: 3, inactiveLabel: false, range:"(0..5)") {
+        valueTile("fanLevel", "device.fanMode", decoration: "flat") {
+            state "level", label:'Fan Level: ${currentValue}'
+        }
+        controlTile("fanSliderControl", "device.fanMode", "slider", height: 1, width: 2) {
                  state "level", action:"sendFanCommand"
+        }
+
+        valueTile("desiredHumidity", "device.desiredHumidity", width: 2, height: 2, decoration: "flat") {
+            state "desiredHumidity", label:'Setpoint: ${currentValue}'
+        }
+
+        valueTile("mainTile", "device.desiredHumidity", width: 2, height: 2, decoration: "flat") {
+            state "desiredHumidity", label:'${currentValue}'
+        }
+
+        valueTile("humidity", "device.humidity", width: 2, height: 2, decoration: "flat") {
+            state "humidity", label:'Current: ${currentValue}%'
+        }
+
+        controlTile("humiditySliderControl", "device.desiredHumidity", "slider", height: 1, width: 2) {
+                 state "desiredHumidity", action:"sendHumidityCommand"
         }
 
 
 
-        main "switch"
+        main "mainTile"
         //details (["switch","cookedTime","time","mode", "refresh"])
-        details (["switch", "refresh", "fanSliderControl"])//"fanMode", "off", "min", "low", "med", "high", "max"])
+        //details (["fanLevel", "fanSliderControl", "desiredHumidity", "humiditySliderControl", "refresh"])//"fanMode", "off", "min", "low", "med", "high", "max"])
+        details (["off", "min", "low", "med", "high", "max", "hum45", "hum50", "hum55", "hum60", "humMax", "humidity", "refresh", "desiredHumidity"])
 		// TODO: define your main and details tiles here
 	}
 }
 
 // parse events into attributes
 def parse(String description) {
-	log.debug "Parsing '${description}'"
+	// log.debug "Parsing '${description}'"
     def evtMessage = parseLanMessage(description)
     def evtHeader = evtMessage.header
     def evtBody = evtMessage.body
@@ -134,14 +205,13 @@ def parse(String description) {
     evtBody = evtBody.replaceAll(~/&lt;/, "<")
     evtBody = evtBody.replaceAll(~/&gt;/, ">")
 
-    log.debug("Header: ${evtHeader}")
-    log.debug("Body: ${evtBody}")
+    // log.debug("Header: ${evtHeader}")
+    // log.debug("Body: ${evtBody}")
 
     if (evtHeader?.contains("SID: uuid:")) {
-        log.debug("found new SID")
 		def sid = (evtHeader =~ /SID: uuid:.*/) ? ( evtHeader =~ /SID: uuid:.*/)[0] : "0"
 		sid -= "SID: uuid:".trim()
-        log.debug "updating subscription sid: ${sid}"
+        log.debug "Subscription updated!  New SID: ${sid}"
     	updateDataValue("subscriptionId", sid)
     }
 
@@ -149,11 +219,82 @@ def parse(String description) {
         log.debug("evtBody: ${evtBody}")
         def body = new XmlSlurper().parseText(evtBody)
         if (body == 0) {
-            log.debug ("SOMETHING WORKED!")
+            log.debug ("Command succeeded!")
+            refresh()
         } else {
-            log.debug(body.Body.GetAttributesResponse.attributeList)
-            // def list = new XmlSlurper().parseText(body.Body.GetAttributesResponse.attributeList)
-            log.debug(list)
+            log.debug("ELSE!: ${body.Body}")
+            // FanMode
+            // DesiredHumidity
+            // CurrentHumidity
+            // WaterAdvise
+            // NoWater
+            // FilterLife
+            // ExpiredFilterTime
+            try {
+                def regex = /FanMode(\d)DesiredHumidity(\d)CurrentHumidity([\d\.]+)WaterAdvise(\d)NoWater(\d)FilterLife(\d+)ExpiredFilterTime(\d)/
+                // def matcher = body.Body =~
+
+                def matchResponse = body.Body =~ /FanMode(\d)DesiredHumidity(\d)CurrentHumidity([\d\.]+)WaterAdvise(\d)NoWater(\d)FilterLife(\d+)ExpiredFilterTime(\d)/
+                // log.debug("mFM: ${matchResponse[0]}")
+                def result = []
+                def fanMode
+                def desiredHumidity
+
+                switch(matchResponse[0][1].toInteger()) {
+                    case 0:
+                        fanMode = "0"
+                        break
+                    case 1:
+                        fanMode = "20"
+                        break
+                    case 2:
+                        fanMode = "40"
+                        break
+                    case 3:
+                        fanMode = "60"
+                        break
+                    case 4:
+                        fanMode = "80"
+                        break
+                    case 5:
+                        fanMode = "100"
+                        break
+                }
+
+                switch(matchResponse[0][2].toInteger()) {
+                    case 0:
+                        desiredHumidity = "45%"
+                        break
+                    case 1:
+                        desiredHumidity = "50%"
+                        break
+                    case 2:
+                        desiredHumidity = "55%"
+                        break
+                    case 3:
+                        desiredHumidity = "60%"
+                        break
+                    case 4:
+                        desiredHumidity = "Max"
+                        break
+                }
+
+                result += createEvent(name: "fanMode", value:fanMode)
+                result += createEvent(name: "desiredHumidity", value:desiredHumidity)
+                result += createEvent(name: "humidity", value:matchResponse[0][3])
+                result += createEvent(name: "waterAdvise", value:matchResponse[0][4])
+                result += createEvent(name: "noWater", value:matchResponse[0][5])
+                result += createEvent(name: "filterLife", value:matchResponse[0][6])
+                result += createEvent(name: "expiredFilterTime", value:matchResponse[0][7])
+
+                log.debug("Result: ${result}")
+                return result
+            } catch (e) {
+                log.debug("Exception")
+            }
+            // if (curFanMode.getCount()) {
+            //     log.debug("Current Fan Mode: ${curFanMode[0]}")
+            // }
         }
 //        result << createEvent(name: value:)
     }
@@ -222,7 +363,6 @@ def poll() {
 
 
 def on() {
-    log.debug("inside on")
     def body = """
     <?xml version="1.0" encoding="utf-8"?>
     <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
@@ -237,49 +377,56 @@ def on() {
 }
 
 
-def fanOff() {
-    sendFanCommand(0)
-}
-def fanMin() {
-    sendFanCommand(1)
-}
-def fanLow() {
-    sendFanCommand(2)
-}
-def fanMed() {
-    sendFanCommand(3)
-}
-def fanHigh() {
-    sendFanCommand(4)
-}
-def fanMax() {
-    sendFanCommand(5)
-}
+def fanOff()  { sendFanCommand("Off")      }
+def fanMin()  { sendFanCommand("Min")      }
+def fanLow()  { sendFanCommand("Low")      }
+def fanMed()  { sendFanCommand("Med")      }
+def fanHigh() { sendFanCommand("High")     }
+def fanMax()  { sendFanCommand("Max")      }
 
-def hum45() {
-    sendHumidityCommand(0)
-}
-def hum50(){
-    sendHumidityCommand(1)
-}
-def hum55(){
-    sendHumidityCommand(2)
-}
-def hum60(){
-    sendHumidityCommand(3)
-}
-def humMax(){
-    sendHumidityCommand(4)
-}
+def hum45()   { sendHumidityCommand("45%") }
+def hum50()   { sendHumidityCommand("50%") }
+def hum55()   { sendHumidityCommand("55%") }
+def hum60()   { sendHumidityCommand("60%") }
+def humMax()  { sendHumidityCommand("Max") }
 
 
 def sendFanCommand(level) {
+    def newLevel
+
+    switch(level) {
+        case "Off":
+        case 0..5:
+            newLevel = 0
+            break
+        case "Min":
+        case 6..27:
+            newLevel = 1
+            break
+        case "Low":
+        case 28..49:
+            newLevel = 2
+            break
+        case "Med":
+        case 50..71:
+            newLevel = 3
+            break
+        case "High":
+        case 72..93:
+            newLevel = 4
+            break
+        case "Max":
+        case 94..100:
+            newLevel = 5
+            break
+    }
+
     def body = """
     <?xml version="1.0" encoding="utf-8"?>
     <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
     <s:Body>
     <u:SetAttributes xmlns:u="urn:Belkin:service:deviceevent:1">
-    <attributeList>&lt;attribute&gt;&lt;name&gt;FanMode&lt;/name&gt;&lt;value&gt;${level}&lt;/value&gt;&lt;/attribute&gt;</attributeList>
+    <attributeList>&lt;attribute&gt;&lt;name&gt;FanMode&lt;/name&gt;&lt;value&gt;${newLevel}&lt;/value&gt;&lt;/attribute&gt;</attributeList>
     </u:SetAttributes>
     </s:Body>
     </s:Envelope>
@@ -288,12 +435,37 @@ def sendFanCommand(level) {
 }
 
 def sendHumidityCommand(level) {
+    def newLevel
+
+    switch(level) {
+        case "45%":
+        case 0..20:
+            newLevel = 0
+            break
+        case "50%":
+        case 21-40:
+            newLevel = 1
+            break
+        case "55%":
+        case 41-60:
+            newLevel = 2
+            break
+        case "60%":
+        case 61-80:
+            newLevel = 3
+            break
+        case "Max":
+        case 81-100:
+            newLevel = 4
+            break
+    }
+
     def body = """
     <?xml version="1.0" encoding="utf-8"?>
     <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
     <s:Body>
     <u:SetAttributes xmlns:u="urn:Belkin:service:deviceevent:1">
-    <attributeList>&lt;attribute&gt;&lt;name&gt;DesiredHumidity&lt;/name&gt;&lt;value&gt;${level}&lt;/value&gt;&lt;/attribute&gt;</attributeList>
+    <attributeList>&lt;attribute&gt;&lt;name&gt;DesiredHumidity&lt;/name&gt;&lt;value&gt;${newLevel}&lt;/value&gt;&lt;/attribute&gt;</attributeList>
     </u:SetAttributes>
     </s:Body>
     </s:Envelope>
@@ -302,7 +474,6 @@ def sendHumidityCommand(level) {
 }
 
 def sendGetAttributesCommand() {
-    log.debug("Sending GetAttributes")
     def body = """
     <?xml version="1.0" encoding="utf-8"?>
     <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
